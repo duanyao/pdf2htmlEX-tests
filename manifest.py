cb5f25178da49ac4bfd3c75712d8cd4cba7830c4
Lu Wang <coolwanglu@gmail.com>
2014-06-29 00:20:30 -0700

    ..

---
+++ manifest.py
@@ -0,0 +1,10 @@
+manifest = [
+  {
+    'file': '1',
+    'html2png_args': '--crop-x 100 --crop-y 150 --crop-w 220 --crop-h 260'
+  },
+  {
+    'file': 'geneve_1564',
+    'html2png_args': '--height 1100'
+  }
+]
